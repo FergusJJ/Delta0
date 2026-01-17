ac5c46ad74bf1319791e4aba946bd81bce05f4a4
FergusJJ <fergus@joinblink.com>
2026-01-17 15:18:10 +0000

    feat: home updates

--- web/src/components/Home/Components/SwapValueProp/SwapValueProp.tsx
+++ web/src/components/Home/Components/SwapValueProp/SwapValueProp.tsx
@@ -1,11 +1,28 @@
 import { FeatureBox } from "@components/FeatureGrid/FeatureBox";
 import { FeatureGrid } from "@components/FeatureGrid/FeatureGrid";
+import { useNavigate } from "react-router-dom";
+import Button from "@components/Button/Button";
+import s from "./SwapValueProp.module.css";
+import { MdOutlineArrowOutward } from "react-icons/md";
+
+function BridgeButton() {
+  const swapLabel = "Bridge Now";
+  const navigate = useNavigate();
+  return (
+    <Button
+      color="var(--accent-green)"
+      bgColor="var(--bg-dark)"
+      onClick={() => navigate("/bridge")}
+      label={swapLabel}
+      icon={<MdOutlineArrowOutward />}
+    />
+  );
+}
 
 export default function SwapValueProp() {
-  const heading = "Transparent Swaps, In App";
+  const heading = "Transparent Cross-Chain Swaps";
   const subheading =
     "Our bridging portal displays the best routes to both swap and bridge at once - and we update you at every step along the way.";
-  const swapCallToAction = "Swap crypto now";
 
   return (
     <div style={{ alignSelf: "start", width: "100%" }}>
@@ -13,12 +30,15 @@ export default function SwapValueProp() {
         <FeatureGrid layout={["1fr", "4fr", "4fr", "1fr"]}>
           <div></div>
           {/**/}
-          <FeatureGrid>
+          <FeatureGrid direction="vertical" layout={["3fr", "1fr"]}>
             <FeatureBox title={heading} description={subheading}></FeatureBox>
+            <div className={s.buttonContainer}>
+              <BridgeButton />
+            </div>
           </FeatureGrid>
           {/**/}
-          <div>
-            <p> image here</p>
+          <div className={s.wrapper}>
+            <img src="/demoplaceholder.png" />
           </div>
           {/**/}
           <div></div>
